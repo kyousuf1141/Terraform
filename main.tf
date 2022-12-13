675df41b6d718d7487838e0a7df265351bcec54e
Kouser Yousuf <v-kyousuf@microsoft.com>
2022-12-13 12:09:30 -0600

    Updated Pipeline file

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "azurerm" {
+    version = "2.92.0"
+    features {}
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tf_rg_blobstore"
+        storage_account_name = "tfstoragekouysuf"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
+resource "azurerm_resource_group" "tf_test"{
+    name ="tfmainrg"
+    location ="Australia East"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
+
+  ip_address_type     = "public"
+  dns_name_label      = "kyousufwa"
+  os_type             = "Linux"
+
+  container {
+      name            = "weatherapi"
+      image           = "kyousuf/weatherapi"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+  }
+}
